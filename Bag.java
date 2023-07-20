a9374133672754354f6d7c2a605656a7ea825e3c
ChandanV45 <chandanv.xworkz@gmail.com>
2023-07-20 11:38:26 +0530

    bag program

---
+++ Bag.java
@@ -0,0 +1,17 @@
+class Bag{
+	public static void main(String[] args)
+	{
+		System.out.println("Bag information");
+		int price=1000;
+		String color1="Black";
+		String brand="American Tourist";
+		byte bagZips=4;
+		String color2="Blue";
+		
+		System.out.println("Price :"+price);
+		System.out.println("Color :"+color1);
+		System.out.println("Color :"+color2);
+        System.out.println("Brand :"+brand);
+		System.out.println("Bag Zips :"+bagZips);
+	}
+}
